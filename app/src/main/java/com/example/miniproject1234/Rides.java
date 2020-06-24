20ceda288fe9c94ff993829997895206b510dd40
deju-006 <dheerajreddy600@gmail.com>
2020-06-24 21:53:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/miniproject1234/Rides.java
@@ -0,0 +1,54 @@
+package com.example.miniproject1234;
+
+public class Rides {
+            public String start,end,quantity,price,tme;
+            public Rides(){
+
+    }
+  public Rides(String start,String end,String quantity,String price,String tme){
+                this.start=start;
+                this.end=end;
+                this.quantity=quantity;
+                this.price=price;
+                this.tme=tme;
+  }
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getTme() {
+        return tme;
+    }
+
+    public void setTme(String tme) {
+        this.tme = tme;
+    }
+}
--- app/src/main/java/com/example/miniproject1234/provideActivity2.java
+++ app/src/main/java/com/example/miniproject1234/provideActivity2.java
@@ -1,11 +1,17 @@
 package com.example.miniproject1234;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -15,23 +21,49 @@ public class provideActivity2 extends AppCompatActivity {
     private AutoCompleteTextView Destination;
     private AutoCompleteTextView amount;
     private AutoCompleteTextView cost;
+    private Button save;
     private EditText time;
     private FirebaseAuth mAuth;
     private DatabaseReference mRideDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_provide2);
-      startpoint=(AutoCompleteTextView) findViewById(R.id.start_point);
-      Destination=(AutoCompleteTextView) findViewById(R.id.destination_point);
-      amount=(AutoCompleteTextView) findViewById(R.id.amount_bags);
-      cost=(AutoCompleteTextView) findViewById(R.id.cost_rs);
-      time= (EditText) findViewById(R.id.editTextTime);
-      mRideDatabase= FirebaseDatabase.getInstance().getReference().child("Rides");
-      mAuth=FirebaseAuth.getInstance();
-      mRideDatabase.child(mAuth.getCurrentUser().getUid()).setValue();
+        startpoint = (AutoCompleteTextView) findViewById(R.id.start_point);
+        Destination = (AutoCompleteTextView) findViewById(R.id.destination_point);
+        amount = (AutoCompleteTextView) findViewById(R.id.amount_bags);
+        cost = (AutoCompleteTextView) findViewById(R.id.cost_rs);
+        time = (EditText) findViewById(R.id.editTextTime);
+        save = (Button) findViewById(R.id.search);
 
+        mRideDatabase = FirebaseDatabase.getInstance().getReference().child("Rides");
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String start = startpoint.getText().toString();
+                final String end = Destination.getText().toString();
+                final String quantity = amount.getText().toString();
+                final String price = cost.getText().toString();
+                final String tme = time.getText().toString();
+
+                mAuth = FirebaseAuth.getInstance();
+                Rides r = new Rides(start, end, quantity, price, tme);
+                mRideDatabase.child(mAuth.getCurrentUser().getUid()).setValue(r).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "uploded successfully", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Unsuccesfull ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
 
 
     }
 }
+
